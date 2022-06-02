ef87e43d08854517a48b4b6c19ab400c55b0fbe6
jamescarr0 <jamescarr0@hotmail.com>
2022-06-02 18:52:45 +0100

    Add unit tests

---
+++ test_libcalc.py
@@ -0,0 +1,10 @@
+import libcalc
+
+
+class TestCalculator:
+
+    def test_addition(self):
+        assert 4 == libcalc.add(2, 2)
+
+    def test_subtraction(self):
+        assert 2 == libcalc.subtract(4, 2)
